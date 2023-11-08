3f8444250bcfdb893d789af3ab8ace23ec814edf
Trevor McKey <mckey.trevor@gmail.com>
2023-11-07 18:42:06 -0600

    bash1.sh

---
+++ Unit 103/bash1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Script: Ops 201 Class 01 Ops Challenge Solution"
+echo "Author: Trevor"        
+echo "Date of latest revision: 7 November 2023"
+echo "Purpose: Print a string to the terminal
