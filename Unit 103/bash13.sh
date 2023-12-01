82d9ea612d598059cf28bfa5414ebfbf28e3ef9f
Trevor McKey <mckey.trevor@gmail.com>
2023-11-30 18:16:31 -0600

    bash13.sh

--- Unit 103/bash13.sh
+++ Unit 103/bash13.sh
@@ -17,3 +17,20 @@
 # % (Remainder)
 # * (Multiply)
 
+#!/bin/bash
+
+echo "Enter a number between 2 and 5: "
+read user_input
+
+# Check if the input is a number
+if [[ $user_input =~ ^[0-9]+$ ]]; then
+    # Check if the number is between 2 and 5 (inclusive)
+    if ((user_input >= 2 && user_input <= 5)); then
+        echo "Valid Number"
+        echo "Your number is $user_input"
+    else
+        echo "Not valid!"
+    fi
+else
+    echo "Invalid input. Please enter a valid number."
+fi
