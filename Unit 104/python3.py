1ab71f7a8800b9d5f1872915da5c78ecbfaa9f9b
Trevor McKey <mckey.trevor@gmail.com>
2023-12-14 17:59:18 -0600

    bash1.sh

--- Unit 104/python3.py
+++ Unit 104/python3.py
@@ -2,6 +2,7 @@
 # Using one of the comparison operators in Python, write a simple program that takes the parameter n as input,
 #  which is an integer, and prints False if n is less than 100, and True if n is greater than or equal to 100.
 
+#Chat GPT's Code
 # def check_greater_than_or_equal_to_100(n):
 #     result = n >= 100
 #     print(result)
@@ -12,6 +13,8 @@
 # # Checking and printing the result
 # check_greater_than_or_equal_to_100(n)
 
+
+#Josh' Code
 # integer = int(input("Enter a Number: "))
 # print("Integer is: " + str(integer))  # Printing the integer entered by the user
 
@@ -21,6 +24,8 @@
 # elif integer < 100:
 #     print("False")
 
+
+#Victor's Code
 n = int(input("Enter a number:"))
 m = 100
 if n >= m:
